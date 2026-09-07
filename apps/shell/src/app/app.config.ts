11ffbe4c42aa78a06cd88d7a7ca1b5ce920e7a90
Lexys Manuel <lexysalonso@gmail.com>
2026-09-07 16:35:36 -0400

    feat(appointments): wire appointments feature into app routing

--- apps/shell/src/app/app.config.ts
+++ apps/shell/src/app/app.config.ts
@@ -1,14 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
 import { appRoutes } from './app.routes';
-import { mockBackendInterceptor, API_URL } from '@proj/core';
-import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(appRoutes, withComponentInputBinding()),
-    provideHttpClient(withInterceptors([mockBackendInterceptor])),
-    { provide: API_URL, useValue: environment.apiUrl },
-  ],
+  providers: [provideRouter(appRoutes, withComponentInputBinding())],
 };
